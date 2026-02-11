fefd00bcd01aad6a25127009fa2e357042b8128e
Kacper Szymański <stefek9113@gmail.com>
2026-02-11 18:28:00 +0100

    feat(shortener): add url model with short code generation

---
+++ shortener/models.py
@@ -0,0 +1,25 @@
+from typing import Any
+
+from django.db import models
+
+from .utils import generate_short_code
+
+
+class ShortenedURL(models.Model):
+    original_url = models.TextField()
+    short_code = models.CharField(max_length=8, unique=True, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args: Any, **kwargs: Any) -> None:
+        if not self.short_code:
+            self.short_code = self._generate_unique_code()
+        super().save(*args, **kwargs)
+
+    def _generate_unique_code(self) -> str:
+        code = generate_short_code()
+        while ShortenedURL.objects.filter(short_code=code).exists():
+            code = generate_short_code()
+        return code
+
+    def __str__(self) -> str:
+        return f"{self.short_code} -> {self.original_url[:50]}"
